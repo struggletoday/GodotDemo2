17f907f9f27c8bf9a720f8e1be2a3a9f45655610
leng <237471224@qq.com>
2025-05-20 16:11:41 +0800

    修改各场景物理碰撞层级

--- Scripts/win_cup.gd
+++ Scripts/win_cup.gd
@@ -2,7 +2,7 @@ extends Area2D
 @export var target_level : PackedScene
 
 func _on_body_entered(body: Node2D) -> void:
-	print(body.name)
+	print("人物名称"+body.name)
 	#检测是玩家就进入下一关
 	if body.name == "Player":
 		#跳转到指定场景
